491db2da0a5be8dfe6e31efb28222ff8a13c2b9f
antheakav <antheakavanagh@gmail.com>
2017-01-18 22:26:51 -0500

    Delete mainak1.js

--- parent_costs/mainak1.js
+++
@@ -1,4 +0,0 @@
-<iframe src="https://vida.io/embed/RgGnRK6YnHQSeYJ7W" width="1000" height="1025" seamless frameBorder="0" scrolling="no"></iframe>
-  
-
-  <script src="https://d3js.org/d3.v4.min.js"></script>
